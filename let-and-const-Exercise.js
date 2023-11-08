bcd6b199b40718c1901fde7101b87e9bd0644d86
Nicole <nicole.minjieli@gmail.com>
2023-11-07 22:03:09 -0500

    first commit

---
+++ let-and-const-Exercise.js
@@ -0,0 +1,13 @@
+// Write an ES2015 Version 
+
+const PI = 3.14; 
+
+//let can be reassigned but not redeclared, var can
+
+// const can not be reassigned or redeclared, var can
+
+// let can be reassigned but const can not
+
+// var is function scope, let and const are block scope
+
+//In JavaScript, when the code is run, the computer goes through the code and moves certain things to the top. These things include variable declarations and function declarations. So, even if you write your code with variables or functions appearing later, JavaScript will hoist them to the top, making them available for use from the beginning of the code.
